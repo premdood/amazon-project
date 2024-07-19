2ac9a4e49e95a8794a85a04cea320e2f6222ee7d
premdood <premdood+github@gmail.com>
2024-07-19 22:11:09 +0530

    Save data and generate HTML for delivery options

---
+++ data/deliveryOptions.js
@@ -0,0 +1,13 @@
+export const deliveryOptions = [{
+    id: '1',
+    deliveryDays: 7,
+    deliveryCents: 0
+}, {
+    id: '2',
+    deliveryDays: 3,
+    deliveryCents: 499
+}, {
+    id: '3',
+    deliveryDays: 1,
+    deliveryCents: 999
+}];
