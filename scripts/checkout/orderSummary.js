c9c44ea22b7f6d4064dd3e05e7dd2a4b36feb63f
premdood <premdood+github@gmail.com>
2024-07-25 12:12:01 +0530

    Create automated tests for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -13,7 +13,7 @@ export function renderOrderSummary() {
     const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
     const dateString = calculateDeliveryDate(deliveryOption);
 
-    cartSummaryHTML += `<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+    cartSummaryHTML += `<div class="cart-item-container js-cart-item-container js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
       Delivery date: ${dateString}
     </div>
@@ -28,7 +28,7 @@ export function renderOrderSummary() {
         <div class="product-price">
           ${formatCurrency(matchingProduct.priceCents)}
         </div>
-        <div class="product-quantity">
+        <div class="product-quantity js-product-quantity-${matchingProduct.id}">
           <span>
             Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
           </span>
@@ -39,7 +39,7 @@ export function renderOrderSummary() {
           <span class="save-quantity-link link-primary js-save-link" data-product-id="${matchingProduct.id}" >
             Save
           </span>
-          <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+          <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
             Delete
           </span>
         </div>
